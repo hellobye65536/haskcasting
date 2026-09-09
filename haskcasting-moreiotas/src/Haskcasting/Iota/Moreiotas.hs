0cf173e737db6226af622a1921b66cf383d935c0
hellobye65536 <boshwang1234@gmail.com>
2026-09-08 22:45:35 -0400

    moreiotas matrix serialization

--- haskcasting-moreiotas/src/Haskcasting/Iota/Moreiotas.hs
+++ haskcasting-moreiotas/src/Haskcasting/Iota/Moreiotas.hs
@@ -4,12 +4,15 @@
 
 module Haskcasting.Iota.Moreiotas where
 
+import Data.List (transpose)
 import Data.Sequence qualified as Seq
 import Data.Text (Text)
 import Data.Text qualified as T
+
 import Haskcasting.ExprLang.TH (mkFragExprInstance)
 import Haskcasting.Fragment (Fragment)
-import Haskcasting.Iota (Iota (..), IotaNumber)
+import Haskcasting.Iota (Iota (..), IotaList (IotaList), IotaNumber (IotaNumber))
+import Haskcasting.Pattern (pattern)
 import Haskcasting.Serialize.A qualified as SA
 
 import Haskcasting.Patterns.Hexcasting
@@ -19,9 +22,41 @@ instance Iota IotaString where
   iotaShow (IotaString s) = "\"" <> s <> "\""
   iotaSerializeA _opt (IotaString s) = Seq.singleton $ SA.IString s
 
+-- list of *rows*
 newtype IotaMatrix = IotaMatrix [[Double]]
 instance Iota IotaMatrix where
   iotaShow _ = "<matrix>"
+  iotaSerializeA opt (IotaMatrix rows)
+    | rows /= transpose cols = error "matrix not rectangular"
+    | [[n]] <- rows =
+        Seq.fromList
+          [ SA.INumber n
+          , SA.IPattern patTransformationPurification
+          , SA.IExec 1 1
+          ]
+    | ([_, _, _] : _) <- cols =
+        foldMap
+          ( \vec -> case vec of
+              [x, y, z] -> Seq.singleton $ SA.IVector x y z
+              _ -> error "column not of length 3"
+          )
+          cols
+          <> Seq.fromList
+            [ SA.IMergeN (length cols)
+            , SA.IPattern patTransformationPurification
+            , SA.IExec 1 1
+            ]
+    | otherwise =
+        iotaSerializeA
+          opt
+          (IotaList $ Seq.fromList $ map (\col -> IotaList $ Seq.fromList $ map IotaNumber col) cols)
+          <> Seq.fromList
+            [ SA.IPattern patTransformationPurification
+            , SA.IExec 1 1
+            ]
+   where
+    cols = transpose rows
+    patTransformationPurification = [pattern| SOUTH_WEST awwaeawwaadwa |]
 
 data IotaItemStack = IotaItemStack Text Int
 instance Iota IotaItemStack where
